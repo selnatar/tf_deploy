9d2492393b6d8ee60f953cbd7a76bf022a6650f0
selnatar <79561064+selnatar@users.noreply.github.com>
2021-10-24 22:35:49 -0400

    Updated config

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2-instance1_oct24" {
   ami                    = "ami-013a129d325529d4d"
-  key_name               = "ec2-2"
+  key_name               = "ec2-3"
   instance_type          = "t2.micro"
   #vpc_security_group_ids = "sg-02b07d64f34b63488"
   subnet_id              = "subnet-053270c3787ee4fe6"
